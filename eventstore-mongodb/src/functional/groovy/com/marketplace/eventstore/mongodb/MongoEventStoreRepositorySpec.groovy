0abb2196b00046182d28e13421a29667be08c834
Richard Agyei <richard.agyei@gmail.com>
2020-12-03 23:12:37 -0600

    Setup functional tests using spock in evenstore-mongodb

--- eventstore-mongodb/src/functional/groovy/com/marketplace/eventstore/mongodb/MongoEventStoreRepositorySpec.groovy
+++ eventstore-mongodb/src/functional/groovy/com/marketplace/eventstore/mongodb/MongoEventStoreRepositorySpec.groovy
@@ -1,17 +1,25 @@
 package com.marketplace.eventstore.mongodb
 
-import spock.lang.Specification
+import com.mongodb.reactivestreams.client.MongoCollection
+import com.mongodb.reactivestreams.client.MongoDatabase
+import org.bson.codecs.configuration.CodecRegistry
 
-class MongoEventStoreRepositorySpec extends Specification {
-    void setup() {
+class MongoEventStoreRepositorySpec extends AbstractContainerInitializerSpec {
+    MongoEventStoreRepository eventStoreRepository;
 
+    def setup() {
+        println "Setting up spec"
+        CodecRegistry registry = provideCodecRegistry();
+        MongoDatabase db =
+                mongoClient.getDatabase(mongoConfig.getDatabase()).withCodecRegistry(registry);
+        MongoCollection<MongoEventEntity> eventCollection =
+                db.getCollection("event", MongoEventEntity.class);
+
+        eventStoreRepository = new MongoEventStoreRepositoryImpl(objectMapper, eventCollection);
     }
 
-    void "test should pass"() {
-        given:
-        boolean a = true
-        
-        then:
-        assert a
+    void "verify that eventCollection is created"() {
+        expect:
+        eventStoreRepository != null;
     }
 }
