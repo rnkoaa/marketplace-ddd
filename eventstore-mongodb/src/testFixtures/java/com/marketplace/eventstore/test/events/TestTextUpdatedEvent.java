cc064ecd0824060b321d472b195970404a0033de
Richard Agyei <richard.agyei@gmail.com>
2020-12-05 11:19:10 -0600

    Tests for deserializing Typed events into actual events

--- eventstore-mongodb/src/testFixtures/java/com/marketplace/eventstore/test/events/TestTextUpdatedEvent.java
+++ eventstore-mongodb/src/testFixtures/java/com/marketplace/eventstore/test/events/TestTextUpdatedEvent.java
@@ -1,9 +1,13 @@
 package com.marketplace.eventstore.test.events;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.marketplace.eventstore.framework.event.Event;
 import org.immutables.value.Value;
 
 @Value.Immutable
+@JsonSerialize(as = ImmutableTestTextUpdatedEvent.class)
+@JsonDeserialize(as = ImmutableTestTextUpdatedEvent.class)
 public interface TestTextUpdatedEvent extends Event {
 
     String getText();
