0b3a28562697eb78cb715b881313db2844f8d056
Richard Agyei <richard.agyei@gmail.com>
2020-12-05 11:24:21 -0600

    Tests for deserializing Typed events into actual events

--- eventstore-mongodb/src/test/java/com/marketplace/eventstore/mongodb/TypedEventTest.java
+++ eventstore-mongodb/src/test/java/com/marketplace/eventstore/mongodb/TypedEventTest.java
@@ -3,8 +3,10 @@ package com.marketplace.eventstore.mongodb;
 import static com.marketplace.eventstore.test.data.TestMongoEvents.objectMapper;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+import com.marketplace.eventstore.framework.event.Event;
 import com.marketplace.eventstore.framework.event.TypedEvent;
 import com.marketplace.eventstore.test.data.TestMongoEvents;
+import com.marketplace.eventstore.test.events.ImmutableTestCreatedEvent;
 import com.marketplace.eventstore.test.events.ImmutableTestTextUpdatedEvent;
 import com.marketplace.eventstore.test.events.ImmutableTestTitleUpdatedEvent;
 import io.vavr.Tuple;
@@ -14,8 +16,12 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TypedEventTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(TypedEventTest.class);
 
     @Test
     void testTypedEventDeserialization() {
@@ -35,21 +41,24 @@ public class TypedEventTest {
         List<TypedEvent> sequenceTypedEvents = TestMongoEvents.sequencedEvents;
         assertThat(sequenceTypedEvents).size().isEqualTo(3);
 
-        List<Object> collect = sequenceTypedEvents.stream()
+        List<Event> collect = sequenceTypedEvents.stream()
             .map(this::deserializeTypedEvent)
             .filter(Try::isSuccess)
             .map(Try::get)
             .sorted(Comparator.comparingInt(it -> it._1))
-            .map(it -> it._2)
+            .map(it -> (Event) it._2)
             .collect(Collectors.toUnmodifiableList());
 
         assertThat(collect).size().isEqualTo(3);
+        assertThat(collect.get(0)).isInstanceOf(ImmutableTestCreatedEvent.class);
         assertThat(collect.get(1)).isInstanceOf(ImmutableTestTitleUpdatedEvent.class);
         assertThat(collect.get(2)).isInstanceOf(ImmutableTestTextUpdatedEvent.class);
     }
 
     Try<Tuple2<Integer, Object>> deserializeTypedEvent(TypedEvent typedEvent) {
         return Try.of(() -> Class.forName(typedEvent.getType()))
+            .onFailure(ex -> logger.info("error retrieving class for {} with exception {}",
+                typedEvent.getType(), ex.getCause()))
             .flatMap(clzz -> Try.of(() -> {
                 Object object = objectMapper.readValue(typedEvent.getEventBody(), clzz);
                 return Tuple.of(typedEvent.getSequenceId(), object);
@@ -66,7 +75,6 @@ public class TypedEventTest {
         Case($(instanceOf(Exception_n.class)), t -> somethingWithException(t))
     ))
     .getOrElse(other);
-
      */
 }
 
