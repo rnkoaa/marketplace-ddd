60726f0ec31b3979d4398c43a05a0ef64770bbe4
Richard Agyei <richard.agyei@gmail.com>
2021-05-08 07:50:57 -0500

    Tests for inserting and retrieving data in sqlite using jooq

--- eventstore-sqlite/src/funcTest/java/com/marketplace/eventstore/jdbc/EventStorePersistenceTest.java
+++ eventstore-sqlite/src/funcTest/java/com/marketplace/eventstore/jdbc/EventStorePersistenceTest.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.Instant;
+import java.util.List;
+import java.util.Map;
 import org.jooq.DSLContext;
 import org.jooq.InsertResultStep;
 import org.jooq.SQLDialect;
@@ -32,25 +34,10 @@ public class EventStorePersistenceTest {
 
     @Test
     void testInsertRecord() {
-//        Event event =
         String aggregateId = "f3864d92-9571-4212-8eb5-90b2f95c9739";
         String eventId = "bf336e78-dc40-44f9-acec-57cf755987d9";
-        String eventData = """
-            {
-            "event_type": "ClassifiedAdCreated"
-            }
-            """;
-        var eventRecord = new EventDataRecord(
-            null,
-            eventId,
-            "ClassifiedAd:" + aggregateId,
-            aggregateId,
-            "ClassifiedAdCreated",
-            1,
-            eventData,
-            Instant.now().toString()
-        );
 
+        var eventRecord = createForInsert(eventId, aggregateId);
         InsertResultStep<EventDataRecord> returning = dslContext.insertInto(EVENT_DATA)
             .set(eventRecord)
             .returning(EVENT_DATA.ID);
@@ -58,17 +45,49 @@ public class EventStorePersistenceTest {
         EventDataRecord eventDataRecord = returning.fetchOne();
         assertThat(eventDataRecord).isNotNull();
         assertThat(eventDataRecord.getId()).isNotNull().isGreaterThan(0);
-//        assertTrue(1, eventDataRecord.getId());
+    }
+
+    @Test
+    void insertMultipleRecords() {
+        String aggregateId = "f3864d92-9571-4212-8eb5-90b2f95c9739";
+        Map<String, String> eventMap = Map.of(
+            "bf336e78-dc40-44f9-acec-57cf755987d9", "CreateClassifiedAd",
+            "361b79f3-bfab-448e-8178-07ef679f34cd", "UpdateClassifiedAdTitle",
+            "b975119e-e09e-42e7-9999-a443758f672e", "AddPictureToClassifiedAd"
+        );
+
+        var eventRecord = createMultipleEvents(eventMap, aggregateId);
+        int[] execute = dslContext.batchStore(eventRecord).execute();
+
+        assertThat(execute).isNotEmpty().hasSize(3);
+    }
+
+    @Test
+    void testInsertMultipleRecords() {
+        String aggregateId = "f3864d92-9571-4212-8eb5-90b2f95c9739";
+        String eventId = "bf336e78-dc40-44f9-acec-57cf755987d9";
+
+        var eventRecord = createForInsert(eventId, aggregateId);
+        InsertResultStep<EventDataRecord> returning = dslContext.insertInto(EVENT_DATA)
+            .set(eventRecord)
+            .returning(EVENT_DATA.ID);
+
+        EventDataRecord eventDataRecord = returning.fetchOne();
+        assertThat(eventDataRecord).isNotNull();
+
+        List<EventDataRecord> eventDataRecords = dslContext.select().from(EVENT_DATA).fetchInto(EventDataRecord.class);
+        assertThat(eventDataRecords).isNotNull().hasSize(1);
     }
 
     @AfterEach
     void cleanup() throws SQLException {
+        dslContext.delete(EVENT_DATA).execute();
         closeConnection(connection);
     }
 
     @BeforeAll
     static void setupAll() {
-
+// use flyway to seed data and create tables
     }
 
     @AfterAll
@@ -76,7 +95,7 @@ public class EventStorePersistenceTest {
 
     }
 
-    private static Connection createConnection(String connectionString) throws SQLException {
+    private static Connection createConnection(final String connectionString) throws SQLException {
         return DriverManager.getConnection(connectionString);
     }
 
@@ -86,5 +105,48 @@ public class EventStorePersistenceTest {
 
     private static void closeConnection(Connection connection) throws SQLException {
         connection.close();
+    }
+
+    List<EventDataRecord> createMultipleEvents(Map<String, String> events, String aggregateId) {
+        String eventDataFormat = """
+            {
+            "event_type": "%s"
+            }
+            """;
+
+        return events.entrySet()
+            .stream()
+            .map(entry -> {
+                String eventData = String.format(eventDataFormat, entry.getValue());
+                return new EventDataRecord(
+                    null,
+                    entry.getKey(),
+                    "ClassifiedAd:" + aggregateId,
+                    aggregateId,
+                    "ClassifiedAdCreated",
+                    1,
+                    eventData,
+                    Instant.now().toString()
+                );
+            })
+            .toList();
+    }
+
+    EventDataRecord createForInsert(String eventId, String aggregateId) {
+        String eventData = """
+            {
+            "event_type": "ClassifiedAdCreated"
+            }
+            """;
+        return new EventDataRecord(
+            null,
+            eventId,
+            "ClassifiedAd:" + aggregateId,
+            aggregateId,
+            "ClassifiedAdCreated",
+            1,
+            eventData,
+            Instant.now().toString()
+        );
     }
 }
--- eventstore-sqlite/src/main/resources/db/schema.sql
+++ eventstore-sqlite/src/main/resources/db/schema.sql
@@ -8,3 +8,12 @@ create table event_data (
     data TEXT NOT NULL,
     created TEXt NOT NULL
 );
+
+CREATE UNIQUE INDEX idx_event_data_event_id
+ON event_data (event_id);
+
+CREATE INDEX idx_event_data_aggregate_id
+ON event_data(aggregate_id);
+
+CREATE INDEX idx_event_data_aggregate_name
+ON event_data(aggregate_name);
