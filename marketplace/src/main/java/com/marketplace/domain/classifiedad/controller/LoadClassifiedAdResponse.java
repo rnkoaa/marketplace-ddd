8a1410da05ca301e77aa6a3f32af5c7bb048442a
Richard Agyei <richard.agyei@gmail.com>
2021-06-01 17:01:21 -0500

    Classified ad load

--- marketplace/src/main/java/com/marketplace/domain/classifiedad/controller/LoadClassifiedAdResponse.java
+++ marketplace/src/main/java/com/marketplace/domain/classifiedad/controller/LoadClassifiedAdResponse.java
@@ -1,6 +1,11 @@
 package com.marketplace.domain.classifiedad.controller;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.marketplace.domain.classifiedad.ClassifiedAdState;
+import com.marketplace.domain.classifiedad.command.UpdateClassifiedAd.PictureDto;
+import com.marketplace.domain.classifiedad.command.UpdateClassifiedAd.PriceDto;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.immutables.value.Value.Immutable;
 
@@ -22,4 +27,16 @@ public interface LoadClassifiedAdResponse {
     UUID getClassifiedAdId();
 
     UUID getOwner();
+
+    Optional<String> getTitle();
+
+    Optional<String> getText();
+
+    Optional<PriceDto> getPrice();
+
+    Optional<UUID> getApprover();
+
+    ClassifiedAdState getState();
+
+    List<PictureDto> getPictures();
 }
